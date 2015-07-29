9778236d50a5fb3742962274425114475da64532
Cam Saul <cam@geotip.com>
2015-07-28 17:57:00 -0700

    add uberdoc :wink:

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
-;; -*- comment-column: 70; -*-
+;; -*- comment-column: 80; -*-
 
 (defproject primes "primes-1.0"
   :description      "Prime Number Multiplacation Table Printer"
   :min-lein-version "2.5.0"
-  :dependencies     [[org.clojure/clojure "1.7.0"]]
+  :dependencies     [[org.clojure/clojure "1.6.0"]]                             ; marginalia chokes on clojure 1.7 :/
   :main             primes.core
-  :profiles         {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.10"]
-                                          [expectations "2.1.2"]
-                                          [marginalia "0.8.0"]]
-                           :plugins      [[cider/cider-nrepl "0.9.1"]
-                                          [lein-expectations "0.0.8"]
-                                          [refactor-nrepl "1.1.0"]]}})
+  :profiles         {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.10"]    ; needed by cider-nrepl
+                                          [expectations "2.1.2"]                ; unit tests
+                                          [marginalia "0.8.0"]]                 ; documentation
+                           :plugins      [[cider/cider-nrepl "0.9.1"]           ; for Emacs development, included here for sake of convenience
+                                          [lein-expectations "0.0.8"]           ; unit tests
+                                          [lein-marginalia "0.8.0"]             ; documentation
+                                          [refactor-nrepl "1.1.0"]]}})          ; extensions to cider-nrepl.
